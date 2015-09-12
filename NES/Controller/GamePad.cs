e7f2101d942ccf33e4dc6d6731cf1667d8db2567
Xma1 <xma1@mail.ru>
2015-09-13 00:21:02 +0200

    Controller

--- NES/Controller/GamePad.cs
+++ NES/Controller/GamePad.cs
@@ -7,5 +7,16 @@ namespace NES.Controller
 {
     class GamePad
     {
+        public class Controller
+        {
+            public bool a, b, SELECT, START, L, R, U, D;
+        }
+
+        public static Controller Player1;
+        public static Controller Player2;
+        public static Controller Player3;
+        public static Controller Player4;
+
+
     }
 }
